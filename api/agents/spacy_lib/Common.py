0d19bb05011f8a4e47627a1a8d2f53396f8a5567
Sunil Patel <126328736+spatelsuy@users.noreply.github.com>
2026-09-20 12:01:29 -0400

    Add generic time unit words to Common.py
    Added a set of generic time unit words for processing.

--- api/agents/spacy_lib/Common.py
+++ api/agents/spacy_lib/Common.py
@@ -4,3 +4,14 @@ from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 import spacy
 import dateparser
+
+
+_GENERIC_UNIT_WORDS = {
+    "second", "seconds",
+    "minute", "minutes",
+    "hour", "hours",
+    "day", "days",
+    "week", "weeks",
+    "month", "months",
+    "year", "years",
+}
