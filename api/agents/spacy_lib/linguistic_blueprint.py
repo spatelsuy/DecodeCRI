bf8009c2cff205e9ec807fe76b269ddf9cc65fd9
Sunil Patel <126328736+spatelsuy@users.noreply.github.com>
2026-09-20 14:21:54 -0400

    Import EntityAnalyzer in linguistic_blueprint.py

--- api/agents/spacy_lib/linguistic_blueprint.py
+++ api/agents/spacy_lib/linguistic_blueprint.py
@@ -20,6 +20,7 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from spacy_lib.Common import *
 from spacy_lib.BaseAnalyzer import BaseAnalyzer
 from spacy_lib.TypoAnalyzer import TypoAnalyzer
+from spacy_lib.EntityAnalyzer import EntityAnalyzer
  
 # ==========================================================
 # LINGUISTIC CONTEXT
